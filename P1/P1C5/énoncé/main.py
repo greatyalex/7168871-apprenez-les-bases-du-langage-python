baac0aab50b34d1b697d0c795ef85e5e3082f2d6
greatyalex <greaty_alex@hotmail>
2025-02-24 11:27:29 +0100

    résultats exo

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,17 @@
-# Écrivez votre code ici !
+nom="alexandre"
+age="47"
+taille="1,92"
+est_étudiant= False
+
+print(f"nom: {nom}")
+print(f"age: {age}")
+print(f"taille : {taille}")
+print(f"est_etudiant : {est_étudiant}")
+
+print(f"type nom: {type(nom)}")
+print(f"type age: {type (age)}")
+print(f"type taille: {type (taille)}")
+print(f"type est_etudiant: {type (est_étudiant)}")
+
+
+
