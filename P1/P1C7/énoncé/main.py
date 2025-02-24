c4fbd614143fa544bf0bc2f2cfa964a9f202da6f
greatyalex <greaty_alex@hotmail>
2025-02-24 16:50:28 +0100

    exo

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,18 @@
-# Écrivez votre code ici !
+Fruits = {"Pomme":"Rouge","Banane":"Jaune","Orange":"Orange"}
+print(Fruits)
+
+Fruits["Kiwi"]="Vert"
+print(Fruits)
+
+Fruits["Banane"]
+couleur_banane = Fruits["Banane"]
+print(couleur_banane)
+
+Fruits["Pomme"] = "Vert"
+print(Fruits)
+
+del Fruits["Banane"]
+print(Fruits)
+
+
+
---
+++ Untitled-2.py
@@ -0,0 +1,13 @@
+nouvelle_campagne ={"responsable_de_campagne":"Jeanne d'Arc","nom_de_campagne":
+"campagne nous aimons les chiens","date_de_début":"01/01/2020","influenceurs_importants":["@MonAmourDeChien","@meilleuresFriandisesPourChiens"]}
+
+taux_de_conversion = {}
+taux_de_conversion['facebook'] = 3.4
+taux_de_conversion['instagram'] = 1.2
+taux_de_conversion = dict()
+taux_de_conversion['facebook'] = 3.4
+taux_de_conversion['instagram'] = 1.2
+
+nouvelle_campagne['responsable_de_campagne']
+"Jeanne d'Arc"
+
