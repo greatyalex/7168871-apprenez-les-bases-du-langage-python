d8e32d0bafd801e93f9f4a67b52b9979d0970b48
greatyalex <greaty_alex@hotmail>
2025-02-24 13:41:24 +0100

    résultat exo

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,19 @@
-# Écrivez votre code ici !
+Fruits = ["pomme", "banane", "orange"]
+print(Fruits)
+
+Fruits.append("kiwi")
+print(Fruits)
+
+Fruits.remove("orange")
+print(Fruits)
+
+Fruits[1]= ("ananas")
+print(Fruits)
+
+len(Fruits)
+
+Fruits.append("orange")
+
+Fruits.sort()
+print(Fruits)
+
